6026528705c37e795269827233b7fe69f3f9e588
Kimleng <kimlenghor2006@gmail.com>
2023-08-23 17:26:51 -0500

    add context to the app

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import FavoritesContext from "../store/favorites-context";
 
 import styles from './MainNavigation.module.css';
 
 function MainNavigation() { 
+
+    const favoritesCtx = useContext(FavoritesContext);
+
     return (
         <header className={styles.header}>
             <h1>React Meetups</h1>
@@ -15,7 +20,9 @@ function MainNavigation() {
                         <Link to="/new-meetup">Add New Meetup</Link>
                     </li>
                     <li>
-                        <Link to="/favorites">My Favorites</Link>
+                        <Link to="/favorites">My Favorites
+                            <span className={ styles.badge }>{ favoritesCtx.totalFavorites }</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,27 @@
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
 import Card from '../ui/Card';
 import styles from './MeetupItem.module.css';
 
 function MeetupItem(props) { 
+
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler() { 
+        if (itemIsFavorite) {
+            favoritesCtx.removeFavorite(props.id)
+        } else { 
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address,
+            })
+        }
+    }
+
     return <li className={styles.item}>
         <Card>
             <div className={styles.image}>
@@ -13,7 +33,7 @@ function MeetupItem(props) {
                 <p>{ props.description }</p>
             </div>
             <div className={ styles.actions }>
-                <button>To Favorites</button>
+                <button onClick={toggleFavoriteStatusHandler}>{ itemIsFavorite ? "Remove from Favorites" : "Add to Favorites"}</button>
             </div>
         </Card>
     </li>
